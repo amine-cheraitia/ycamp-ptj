1c11af4df20f2cfab20c792a7182700716a76a32
amine-cheraitia <amine-cheraitia@hotmail.com>
2024-09-13 09:37:14 +0200

    added relationships region & departement

--- Api/app/Models/Region.php
+++ Api/app/Models/Region.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Region extends Model
 {
     use HasFactory;
+
+    public function departements(): HasMany
+    {
+        return $this->hasMany(Department::class);
+    }
 }
